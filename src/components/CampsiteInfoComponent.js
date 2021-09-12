59b55fc88efe83b74c028679c80a7097e3f014f4
Nick Workman <nicholaszworkman@gmail.com>
2021-09-11 22:40:20 -0400

    first commit

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Label, FormGroup, Row, Modal } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import Button from 'reactstrap/lib/Button';
+import ModalHeader from 'reactstrap/lib/ModalHeader';
+import ModalBody from 'reactstrap/lib/ModalBody';
+import { Control, LocalForm, Errors } from 'react-redux-form';
 
-function RenderCampsite({campsite}) {
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
+
+function RenderCampsite({ campsite }) {
     return (
         <div className="col-md-5 m-1">
             <Card>
@@ -15,7 +22,7 @@ function RenderCampsite({campsite}) {
     );
 }
 
-function RenderComments({comments}) {
+function RenderComments({ comments }) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
@@ -26,12 +33,13 @@ function RenderComments({comments}) {
                             <div key={comment.id}>
                                 <p>
                                     {comment.text}<br />
-                                    -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                                    -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}
                                 </p>
                             </div>
                         );
                     })
                 }
+                <CommentForm />
             </div>
         );
     }
@@ -62,4 +70,92 @@ function CampsiteInfo(props) {
     return <div />;
 }
 
+class CommentForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isModalOpen: false
+        };
+
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleComment = this.handleComment.bind(this);
+
+    }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        })
+    }
+
+    handleComment(values) {
+        alert(`Rating: ${values.rating} Your Name: ${values.author} Comment: 
+        ${values.comment}`);
+        this.toggleModal();
+        console.log(values);
+    }
+
+    render() {
+        return (
+            <>
+                <Button outline className="fa-lg" onClick={this.toggleModal}>
+                    <i className="fa fa-pencil" /> Submit Comment
+                </Button>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader>Submit Comment</ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={this.handleComment}>
+                            <div className="form-group">
+                                <Label htmlFor="rating">Rating</Label>
+                                <Control.select model=".rating" id="rating" name="rating" className="form-control">
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                </Control.select>
+                            </div>
+
+                            <div className="form-group">
+                                <Label htmlFor="author">Author</Label>
+                                <Control.text model=".author" id="author" name="author"
+                                    placeholder="Author"
+                                    className="form-control"
+                                    validators={{
+                                        minLength: minLength(2),
+                                        maxLength: maxLength(15)
+                                    }}
+                                />
+                                <Errors
+                                    className="text-danger"
+                                    model=".author"
+                                    show="touched"
+                                    component="div"
+                                    messages={{
+                                        minLength: 'Must be at least 2 characters',
+                                        maxLength: 'Must be 15 characters or less.'
+                                    }}
+                                />
+                            </div>
+                            
+                            <div className="form-group">
+                                <Label htmlFor="comment">Comment</Label>
+                                <Control.textarea model=".comment" id="comment" name="comment"
+                                    rows="6"
+                                    className="form-control"
+                                />
+                            </div>
+
+                            <div className="form-group">
+                                <Button type="submit" value="submit" variant="primary">Submit</Button>
+                            </div>
+
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+            </>
+        );
+    };
+}
 export default CampsiteInfo;
